ae82a917470ebc26b5cdb41f019c6a8924dd1685
Bekir Bilgili <mbekirbilgili@gmail.com>
2024-02-21 09:43:25 +0300

    Sicaklıga gore etkinlik Eklendi

---
+++ src/SicakligaGoreEtkinlikOnerme.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class SicakligaGoreEtkinlikOnerme {
+    public static void main(String[] args) {
+
+        int heat;
+
+        Scanner input = new Scanner (System.in);
+        System.out.print("Hava sıcaklığını girin: ");
+
+        if (heat < 5) {
+            System.out.print("Kayak Yapabilirsiniz.");
+        }else if (heat >= 5 && heat < 25) {
+            if(heat <=15) {
+                System.out.println("Sinemaya gidebilirsin");
+            }
+            if (heat <= 15) {
+                System.out.println("Sinemaya gidebilirsin");
+            }
+            if (heat >=10) {
+                System.out.println("Pikniğe gidebilirsin");
+            }
+        }else {
+            System.out.println("Yüzmeye gidebilirin");
+        }
+    }
+}
